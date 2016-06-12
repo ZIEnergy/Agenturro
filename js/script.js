45d646af57e2fc1b801016f5c1b8f8a8a10ff9a0
Дмитрий Жур <tehzienergy@gmail.com>
2016-06-12 20:26:56 +0300

    footer

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(document).ready(function() {
-    
+    $('.projects__slider').slick({
+      dots: true,
+    });
+  
+    $('.portfolio').slick({
+      infinite: true,
+      variableWidth: true
+    });
 });
