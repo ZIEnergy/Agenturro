60a30b9c368ceb0fb641534d224151ed664cd4ef
Дмитрий Жур <tehzienergy@gmail.com>
2016-06-13 00:49:27 +0300

    AG: portfolio slider

--- js/script.js
+++ js/script.js
@@ -5,16 +5,16 @@ $(document).ready(function() {
     });
   
     $('.portfolio').slick({
-      infinite: true,
-      variableWidth: true
+      infinite: false,
+      slidesToShow: 4,
     });
   
     $(window).scroll(function() {
       if (($(window).scrollTop() >= 50)) {
-        $('.sticky-header').fadeIn('fast').css('display', 'flex');
+        $('.sticky-header').fadeIn().css('display', 'flex');
       }
       else if (($(window).scrollTop() <50)) {
-        $('.sticky-header').hide();
+        $('.sticky-header').fadeOut();
       }
     });
   
