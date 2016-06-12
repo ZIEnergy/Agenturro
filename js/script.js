f10838a4a4c479cade1ce39eb34979b619c8cf37
Дмитрий Жур <tehzienergy@gmail.com>
2016-06-13 00:35:06 +0300

    AG: sliders

--- js/script.js
+++ js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function() {
     $('.projects__slider').slick({
       dots: true,
+      slidesToShow: 1,
     });
   
     $('.portfolio').slick({
       infinite: true,
       variableWidth: true
     });
+  
+    $(window).scroll(function() {
+      if (($(window).scrollTop() >= 50)) {
+        $('.sticky-header').fadeIn('fast').css('display', 'flex');
+      }
+      else if (($(window).scrollTop() <50)) {
+        $('.sticky-header').hide();
+      }
+    });
+  
 });
