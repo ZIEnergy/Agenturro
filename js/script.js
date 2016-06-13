480b04b108c292826aa845d1c19300190d4f0fc1
Дмитрий Жур <tehzienergy@gmail.com>
2016-06-13 09:43:18 +0300

    AG: dropzone

--- js/script.js
+++ js/script.js
@@ -1,31 +1,27 @@
 $(document).ready(function() {
-    $('.projects__slider').slick({
-      dots: true,
-      slidesToShow: 1,
-    });
-  
-    $('.portfolio__content').slick({
-      variableWidth: true
-    });
-  
-    $(window).scroll(function() {
-      if (($(window).scrollTop() >= 50)) {
-        $('.sticky-header').fadeIn().css('display', 'flex');
-      }
-      else if (($(window).scrollTop() <50)) {
-        $('.sticky-header').fadeOut();
-      }
-    });
-  
-    $(function () {
-        $('#fileupload').fileupload({
-            dataType: 'json',
-            done: function (e, data) {
-                $.each(data.result.files, function (index, file) {
-                    $('<p/>').text(file.name).appendTo(document.body);
-                });
-            }
-        });
-    });
+  $('.projects__slider').slick({
+    dots: true,
+    slidesToShow: 1,
+  });
+
+  $('.portfolio__content').slick({
+    variableWidth: true
+  });
+
+  $(window).scroll(function() {
+    if (($(window).scrollTop() >= 50)) {
+      $('.sticky-header').fadeIn().css('display', 'flex');
+    }
+    else if (($(window).scrollTop() <50)) {
+      $('.sticky-header').fadeOut();
+    }
+  });
+
+  $(".form-upload__dragndrop").dropzone({ 
+    url: "/file/post",
+    uploadMultiple: true,
+    createImageThumbnails: false
+  });
+  Dropzone.autoDiscover = false;
   
 });
